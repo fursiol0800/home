6515ce2816f9e486f0bbcf4decf8ec1f9680fbc2
maotseantonio <thetzinantonio@gmail.com>
2024-11-03 17:36:10 +0630

    new

--- wezterm-bak/config/init.lua
+++
@@ -1,7 +0,0 @@
-return require("utils.fn").tbl_merge(
-  (require "config.gpu"),
-  (require "config.appearance"),
-  (require "config.font"),
-  (require "config.tab-bar"),
-  (require "config.general")
-)
--- wezterm-bak/mappings/init.lua
+++
@@ -1,4 +0,0 @@
-return require("utils.fn").tbl_merge(
-  (require "mappings.default"),
-  (require "mappings.modes")[1]
-)
