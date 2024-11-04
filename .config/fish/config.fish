b03203e3f1b19ab5dfb9ed1a6b434844739f73c2
maotseantonio <thetzinantonio@gmail.com>
2024-11-04 12:32:47 +0630

    fish edit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 source ~/.config/fish/user_variables.fish
 source ~/.config/fish/abbreviations.fish
 source ~/.config/fish/aliases.fish
-#oh-my-posh init fish --config $HOME/.config/ohmyposh/bubblesextra.omp.json | source
+oh-my-posh init fish --config $HOME/.config/oh-my-posh/bubblesextra.omp.json | source
