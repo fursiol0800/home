a253550ad49e08d59078da3a9249829bc029421e
installer <installer@gmail.com>
2024-11-25 07:24:00 +0630

    new

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 source ~/.config/fish/user_variables.fish
 source ~/.config/fish/abbreviations.fish
 source ~/.config/fish/aliases.fish
-oh-my-posh init fish --config $HOME/.config/oh-my-posh/emodipt-extend.omp.json | source
+#oh-my-posh init fish --config $HOME/.cache/oh-my-posh/themes/montys.omp.json | source
--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,4 +1,4 @@
-function l --wraps='eza --long --header -a --icons --git --group-directories-first' --description 'alias l=eza --long --header -a --icons --git --group-directories-first'
+function lh --wraps='eza --long --header -a --icons --git --group-directories-first' --description 'alias l=eza --long --header -a --icons --git --group-directories-first'
     if type -f eza &>/dev/null
         eza --long --header -a --icons --git --group-directories-first --hyperlink $argv
     else
