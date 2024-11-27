5f522b390c983447ecd5a92c3f6f030cb79c9552
installer <installer@gmail.com>
2024-11-27 12:40:38 +0630

    addsomecode

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,6 +1,6 @@
-function lh --wraps='eza --long --header -a --icons --git --group-directories-first' --description 'alias l=eza --long --header -a --icons --git --group-directories-first'
+function l --wraps='eza --long --header --icons --git --group-directories-first' --description 'alias l=eza --long --header --icons --git --group-directories-first'
     if type -f eza &>/dev/null
-        eza --long --header -a --icons --git --group-directories-first --hyperlink $argv
+        eza --long --header --icons --git --group-directories-first --hyperlink $argv
     else
         missing_package eza
     end
--- .config/fish/functions/lh.fish
+++ .config/fish/functions/lh.fish
@@ -1,4 +1,4 @@
-function l --wraps='eza --long --header --icons --git --group-directories-first' --description 'alias lh=eza --long --header --icons --git --group-directories-first'
+function lh --wraps='eza --long --header --icons --git --group-directories-first' --description 'alias lh=eza --long --header --icons --git --group-directories-first'
     if type -f eza &>/dev/null
         eza --long --header --icons --git --group-directories-first --hyperlink $argv
     else
